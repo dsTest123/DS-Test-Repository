400a120606d3436d3a961a4a8d7922bfbc94074c
Dominik Schweiger <schweiger.d@gmail.com>
2011-10-26 11:23:58 +0200

    edited label in main form

--- Hello World/Hello World/Form1.Designer.cs
+++ Hello World/Hello World/Form1.Designer.cs
@@ -36,9 +36,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(55, 77);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(28, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Test";
             // 
             // Form1
             // 
